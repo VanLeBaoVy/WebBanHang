207b6333b9a70fe3efc751630743d72a9c0930ef
nghia <trantnghia.911@gmail.com>
2025-03-26 23:20:40 +0700

    Chưa co phan san phan vơi thanh toan

---
+++ static/js/branchs.js
@@ -0,0 +1,162 @@
+const rootBranch = [
+  {
+    title: "Bóng thể thao",
+    id: "1",
+    branch: [
+      {
+        title: "Bóng rổ",
+        id: "1_1",
+      },
+      {
+        title: "Bóng đá",
+        id: "1_2",
+      },
+      {
+        title: "Bóng chuyền",
+        id: "1_3",
+      },
+      {
+        title: "Bóng bàn",
+        id: "1_4",
+      },
+      {
+        title: "Bóng tennis",
+        id: "1_5",
+      },
+      {
+        title: "Bóng golf",
+        id: "1_6",
+      },
+      {
+        title: "Bóng bầu dục",
+        id: "1_7",
+      }
+    ],
+  },
+  {
+    title: "Dụng cụ tập luyện & thể hình",
+    id: "2",
+    branch: [
+      {
+        title: "Tạ tay, tạ đòn, tạ bình vôi",
+        id: "2_1",
+      },
+      {
+        title: "Dây kháng lực",
+        id: "2_2",
+      },
+      {
+        title: "Dây nhảy",
+        id: "2_3",
+      },
+      {
+        title: "Thảm tập yoga",
+        id: "2_4",
+      },
+      {
+        title: "Găng tay tập gym",
+        id: "2_5",
+      },
+      {
+        title: "Máy chạy bộ, xe đạp tập",
+        id: "2_6",
+      },
+    ],
+  },
+  {
+    title: "Dụng cụ thể thao dưới nước",
+    id: "3",
+    branch: [
+      {
+        title: "Kính bơi",
+        id: "3_1",
+      },
+      {
+        title: "Phao bơi",
+        id: "3_2",
+      },
+      {
+        title: "Chân vịt",
+        id: "3_3",
+      },
+      {
+        title: "Ván lướt sóng",
+        id: "3_4",
+      },
+      {
+        title: "Ván chèo SUP",
+        id: "3_5",
+      }
+    ],
+  },
+  {
+    title: "Dụng cụ cầu lông, tennis, bóng bàn",
+    id: "4",
+    branch: [
+      {
+        title: "Vợt cầu lông",
+        id: "4_1",
+      },
+      {
+        title: "Vợt tennis",
+        id: "4_2",
+      },
+      {
+        title: "Vợt bóng bàn",
+        id: "4_3",
+      },
+      {
+        title: "Lưới cầu lông, lưới tennis",
+        id: "4_4",
+      },
+      {
+        title: "Bàn bóng bàn",
+        id: "4_5",
+      },
+    ],
+  },
+  {
+    title: "Dụng cụ thể thao đối kháng",
+    id: "5",
+    branch: [
+      {
+        title: "Bao cát, bao đấm bốc",
+        id: "5_1",
+      },
+      {
+        title: "Găng tay boxing, MMA",
+        id: "5_2",
+      },
+      {
+        title: "Đích đấm, đích đá",
+        id: "5_3",
+      },
+      {
+        title: "Bảo hộ răng, bảo hộ đầu",
+        id: "5_4",
+      }
+    ],
+  },
+  {
+    title: "Trang phục & phụ kiện thể thao",
+    id: "6",
+    branch: [
+      {
+        title: "Quần áo thể thao",
+        id: "6_1",
+      },
+      {
+        title: "Giày thể thao",
+        id: "6_2",
+      },
+      {
+        title: "Băng cổ tay, băng đầu gối",
+        id: "6_3",
+      },
+      {
+        title: "Đồng hồ theo dõi sức khỏeo",
+        id: "6_4",
+      },
+    ],
+  },
+];
